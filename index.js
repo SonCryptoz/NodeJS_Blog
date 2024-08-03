623cc2997ff6b83be25038d007005012efaf0aff
SonCryptoz <118987900+SonCryptoz@users.noreply.github.com>
2024-08-03 16:46:59 +0700

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+// morgan
+app.use(morgan('combined'))
+
+// định nghĩa route '/'
+app.get('/', (req, res) => {
+    return res.send('Hello World!')
+})
+
+
+// 127.0.0.1 - localhost
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`))

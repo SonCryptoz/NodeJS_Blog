74625fd33c5a599e07131f3e10b154b5758f0158
SonCryptoz <118987900+SonCryptoz@users.noreply.github.com>
2024-08-03 22:32:54 +0700

    project changed

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const morgan = require('morgan');
+
+// Sử dụng destructuring để chỉ lấy thuộc tính 'engine' trong object
+const {engine} = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+// đi tới thư mục public chứa các file tĩnh
+app.use(express.static(path.join(__dirname, 'public')));
+
+// morgan(HTTP logger)
+app.use(morgan('combined'));
+
+// template engine
+app.engine('hbs', engine({
+    extname: '.hbs' // định dạng file cho template đang sử dụng
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+// định nghĩa route '/'
+app.get('/home', (req, res) => {
+   res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+ });
+
+// 127.0.0.1 - localhost
+app.listen(port, () => 
+    console.log(`Example app listening at http://localhost:${port}`));
+
+
+

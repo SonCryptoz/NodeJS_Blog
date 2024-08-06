2bd4a93fa3a2bc2dda6145652f55b076e8c943f4
SonCryptoz <118987900+SonCryptoz@users.noreply.github.com>
2024-08-06 21:13:48 +0700

    prettier code x3

--- src/index.js
+++ src/index.js
@@ -1,39 +1,39 @@
 const path = require('path');
 
 const express = require('express');
-  const morgan = require('morgan');
+const morgan = require('morgan');
 
 // Sử dụng destructuring để chỉ lấy thuộc tính 'engine' trong object
-               const { engine } = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
-    const app = express();
+const app = express();
 const port = 3000;
 
-    const route = require('./routes');
+const route = require('./routes');
 
 // đi tới thư mục public chứa các file tĩnh
 app.use(express.static(path.join(__dirname, 'public')));
 
 // xử lý form submit POST để lấy dữ liệu
 app.use(
-                    express.urlencoded({
+    express.urlencoded({
         extended: true,
     }),
 );
-                    app.use(express.json());
+app.use(express.json());
 
 // morgan(HTTP logger)
 // app.use(morgan('combined'));
 
 // template engine
-app.engine(
-        'hbs',
-    engine({
-               extname: '.hbs', // định dạng file cho template đang sử dụng
+        app.engine(
+    'hbs',
+        engine({
+        extname: '.hbs', // định dạng file cho template đang sử dụng
     }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+        app.set('view engine', 'hbs');
+    app.set('views', path.join(__dirname, 'resources/views'));
 
 // Routes init
 route(app);
